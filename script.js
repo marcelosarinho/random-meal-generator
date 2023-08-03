fe689ad52ab457442d889d6b99b19a6ffc5cd414
Marcelo Sarinho <marceloactsarinho@gmail.com>
2023-08-02 22:43:51 -0300

    finalizacao do projeto, so falta responsividade

--- script.js
+++ script.js
@@ -8,65 +8,52 @@ mealButton.addEventListener("click", async () => {
     "https://www.themealdb.com/api/json/v1/1/random.php"
   );
   data = await response.json();
-  console.log(data.meals[0]);
   createMeal(data.meals[0]);
 });
 
 const createMeal = (meal) => {
   const ingredients = [];
 
-  for (let i = 0; i < 20; i++) {
-    if (!meal[`strIngredient${i}`]) break;
+  for (let i = 1; i <= 20; i++) {
+    if (!meal[`strIngredient${i}`]) {
+      break;
+    }
     ingredients.push(
       `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
     );
   }
 
   mealDiv.innerHTML = `<div class="left">
-  <img src="${meal.strThumb}" alt="Imagem da refeição" />
+  <img src="${meal.strMealThumb}" alt="Imagem da refeição" />
 
-  <p><strong>Área:</strong> ${meal.strArea}</p>
-  <p><strong>Categoria:</strong> ${meal.strCategory ?? 'None'}</p>
-  <p><strong>Tags:</strong> ${}</p>
+  <p><strong>Área:</strong> ${meal.strArea ?? "None"}</p>
+  <p><strong>Categoria:</strong> ${meal.strCategory ?? "None"}</p>
+  <p><strong>Tags:</strong> ${
+    meal.strTags ? meal.strTags.split(",").join(", ") : "None"
+  }</p>
 
   <div class="ingredients">
     <h3>Ingredients</h3>
     <ul>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
-      <li>1</li>
+    ${ingredients.map((ingredient) => `<li>${ingredient}</li>`).join("")}
     </ul>
   </div>
 </div>
 
 <div class="right">
-  <h2 class="meal-title">Título</h2>
+  <h2 class="meal-title">${meal.strMeal}</h2>
 
   <div class="instructions">
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
-      provident atque sapiente, nam dolores suscipit voluptatem unde et.
-      Odio earum deleniti, facere a, delectus hic rem deserunt beatae
-      magni quaerat quidem veritatis! Iusto sunt dicta minima fugit quas
-      aperiam aliquid consectetur hic inventore saepe distinctio ipsum
-      voluptas minus recusandae consequuntur eveniet adipisci odio,
-      delectus iste. Consequuntur enim atque illo nemo qui quasi suscipit,
-      repellat nihil id doloribus natus voluptatum cupiditate? Labore
-      aliquam ratione laudantium? Ex corporis officia quam? Quisquam,
-      eius!
+      ${meal.strInstructions}
     </p>
   </div>
 </div>
 
-<div class="meal-video">
-  <a href=""></a>
-</div>
-
-<div class="meal-source">
-  <h4>Source: </h4><a href="">Oi</a>
-</div>`;
+<footer>
+  <h4>Video</h4>
+  <iframe height="300" width="600" src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">Video</iframe>
+  <h4>Source: </h4><a href="">${meal.strSource ?? "None"}</a>
+</footer>
+`;
 };
-
-// mealDiv.innerHTML =
